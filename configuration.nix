4fac6c15fdae623410e4160519dffdcc88914642
Joseph Heyburn <jdheyburn@gmail.com>
2021-12-31 12:28:05 +0000

    Add working caddy build - untested config yet

--- configuration.nix
+++ configuration.nix
@@ -62,7 +62,8 @@
 
   # Use 1GB of additional swap memory in order to not run out of memory
   # when installing lots of things while running other things at the same time.
-  swapDevices = [ { device = "/swapfile"; size = 1024; } ];
+  # JH - increased to 2GB to allow for custom-caddy.nix build
+  swapDevices = [ { device = "/swapfile"; size = 2048; } ];
 
   # All my stuff now
 
