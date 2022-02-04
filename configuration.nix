a284ad7336f34f88578cf0a30c7fa8ab6977736a
Joseph Heyburn <jdheyburn@gmail.com>
2022-02-04 08:25:11 +0000

    Some networking refactor and install kid3

--- configuration.nix
+++ configuration.nix
@@ -35,6 +35,8 @@
     jq
     htop
     fzf
+    python39
+    kid3
   ];
 
   # File systems configuration for using the installer's partition layout
@@ -69,7 +71,37 @@
 
   # All my stuff now
 
-  networking.hostName = "dee";
+  networking = {
+    hostName = "dee";
+    #domain = "joannet.casa";
+    #useDHCP = false;
+
+   #interfaces.eth0.ipv4.addresses = [ {
+   #  address = "192.168.1.10";
+   #  prefixLength = 24;
+   #} ];
+   #defaultGateway = "192.168.1.1";
+   #nameservers = [
+   #  "192.168.1.10"
+   #  "1.1.1.1"
+   #];
+
+   firewall = {
+     allowedTCPPorts = [
+       2049 # NFS
+       111 # NFS
+       80 # Caddy
+       443 # Caddy
+       53 # DNS server
+     ];
+     allowedUDPPorts = [ 
+       53 # DNS server
+       111 # NFS
+       2049 # NFS
+     ];
+   };
+  };
+
   time.timeZone = "Europe/London";
 
   users.mutableUsers = false;
@@ -86,18 +118,26 @@
   };
 
   services.nfs.server.enable = true;
+  # couldn't get 1.25 to work on macos, leaving here so i can see what did and didn't work
   services.nfs.server.exports = ''
-    /mnt/usb 192.168.2.69(rw,nohide,insecure) 192.168.2.10(rw,nohide,insecure)
+    /mnt/usb 192.168.1.20(rw,nohide,insecure) 192.168.1.25(rw,nohide,insecure,no_subtree_check,all_squash,anonuid=1001,anongid=1001) 192.168.1.26(rw,nohide,insecure)
   '';
-  networking.firewall.allowedTCPPorts = [ 
-    2049 # NFS
-    80 # Caddy
-    443 # Caddy
-    53 # DNS server
-  ];
-  networking.firewall.allowedUDPPorts = [ 
-    53 # DNS server
-  ];
+
+  services.samba = {
+    enable = false;
+    openFirewall = true;
+    shares = { usb = {
+      path = "/mnt/usb";
+      writeable = "yes";
+      "force user" = "root";
+      "force group" = "root";
+      "guest ok" = "yes";
+    }; };
+    extraConfig = ''
+      hosts allow = 192.168.1.20 192.168.1.25 192.168.1.25 localhost
+      hosts deny = 0.0.0.0/0
+    '';
+  };
 
 
 
