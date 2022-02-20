f8c36a17dd320ca06400c4a5b1dc684d22fb0dc5
Joseph Heyburn <jdheyburn@gmail.com>
2022-02-20 15:26:43 +0000

    Working prom with scrape

--- modules/prometheus-stack.nix
+++ modules/prometheus-stack.nix
@@ -11,6 +11,26 @@ in {
     enable = true;
     domain = "grafana.svc.joannet.casa";
     port = grafanaPort;
-    addr = "127.0.0.1";
+    addr = "0.0.0.0";
+  };
+
+ services.prometheus = {
+    enable = true;
+    port = 9001;
+    exporters = {
+      node = {
+        enable = true;
+        enabledCollectors = [ "systemd" ];
+        port = 9002;
+      };
+    };
+    scrapeConfigs = [
+      {
+	job_name = "dennis";
+	static_configs = [{
+	  targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.node.port}" ];
+	}];
+      }
+    ];
   };
 }
