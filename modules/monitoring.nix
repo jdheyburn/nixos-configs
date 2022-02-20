d6af809ed25a80ea44f2526bd13c0e3af39d5a9f
Joseph Heyburn <jdheyburn@gmail.com>
2022-02-20 16:12:33 +0000

    Open prom port

--- modules/monitoring.nix
+++ modules/monitoring.nix
@@ -1,6 +1,10 @@
 { config, pkgs, lib, ... }:
 
 {
+
+  ## TODO might not be needed to open for localhost polling on dennis
+  networking.firewall.allowedTCPPorts = [ 9092 ];
+
   services.prometheus = {
     exporters = {
       node = {
