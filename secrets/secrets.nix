9dc6f80fc44e9233aeb42b72c5fffe731c602085
Joseph Heyburn <jdheyburn@gmail.com>
2022-04-28 09:29:41 +0100

    agenix secrets include jdheyburn

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,16 @@
 let
-  deeHost =
+  dee =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIELgWHUsrtcGcZ2A/IlBTuRtvE5lcL7n6PGIEHEXW81k";
+
+  servers = [
+    dee
+  ];
+
+  jdheyburn = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIozTzNnp+KQAwlMUdJuIcvuQmM+Dz5wlB7H15Qx3iZT"
+  ];
+  users = jdheyburn;
 in {
-  "caddy-environment-file.age".publicKeys = [ deeHost ];
+  "caddy-environment-file.age".publicKeys = servers ++ users;
 }
+
