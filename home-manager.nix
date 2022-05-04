1fd4636f4290917b256d7a3b7c1c7ef2ba1abe73
Joseph Heyburn <jdheyburn@gmail.com>
2022-05-04 06:48:44 +0100

    Add git diff-so-fancy

--- home-manager.nix
+++ home-manager.nix
@@ -5,6 +5,11 @@
     enable = true;
     userName = "Joseph Heyburn";
     userEmail = "jdheyburn@gmail.com";
+    extraConfig = {
+      core = {
+        pager = "${pkgs.diff-so-fancy}/bin/diff-so-fancy | less -RF";
+      };
+    };
   };
 
   programs.zsh = {
