88e2c04dbb3d79b6c2b66cfc344d1f6e4cfee24a
Joseph Heyburn <jdheyburn@gmail.com>
2022-06-30 09:01:29 +0100

    Disable backup and loki config to nix

--- hosts/dennis/hardware-configuration.nix
+++ hosts/dennis/hardware-configuration.nix
@@ -20,11 +20,11 @@
     fsType = "ext4";
   };
 
-  fileSystems."/mnt/nfs" = {
-    device = "192.168.1.10:/mnt/nfs";
-    fsType = "nfs";
-    options = [ "x-systemd.automount" "noauto" ];
-  };
+#  fileSystems."/mnt/nfs" = {
+#    device = "192.168.1.10:/mnt/nfs";
+#    fsType = "nfs";
+#    options = [ "x-systemd.automount" "noauto" ];
+#  };
 
   swapDevices = [ ];
 
