71ebeecb8acbdd8a1ff8643619d52d3991f2ccc8
Joseph Heyburn <jdheyburn@gmail.com>
2022-07-18 19:44:50 +0100

    Passwordless sudo

--- hosts/dennis/configuration.nix
+++ hosts/dennis/configuration.nix
@@ -43,6 +43,19 @@ in {
 
   nix.settings.require-sigs = false;
 
+  security.sudo.extraRules = [
+    {
+      users = [ "jdheyburn" ];
+      commands = [
+        {
+          command = "ALL";
+      options = [ "NOPASSWD" ];
+
+        }
+      ];
+    }
+  ];
+
   ###############################################################################
   ## Modules
   ###############################################################################
