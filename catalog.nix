733615436ea35138f71777f65afaa72dd47017b8
Joseph Heyburn <34041368+jdheyburn@users.noreply.github.com>
2022-12-29 10:56:38 +0000

    Dashy config refactor (#40)
    * Some minor refactoring
    
    * dashy refactor init
    
    * Refactor dashy config to be tidier

--- catalog.nix
+++ catalog.nix
@@ -43,7 +43,7 @@
     value = (nodesBase."${hostName}" // { hostName = hostName; });
   }) (builtins.attrNames nodesBase));
 
-  services = {
+  servicesBase = {
     adguard = {
       host = nodes.dee;
       port = 3000;
@@ -52,6 +52,8 @@
       dashy.description = "DNS resolver";
       dashy.icon = "hl-adguardhome";
     };
+
+    blackboxExporter = { port = 9115; };
 
     healthchecks = {
       host = nodes.dee;
@@ -191,4 +193,10 @@
       dashy.icon = "https://avatars.githubusercontent.com/u/43720803";
     };
   };
+
+  # Enrich servicesBase by adding the key as the name - DRY
+  services = builtins.listToAttrs (map (serviceName: {
+    name = serviceName;
+    value = (servicesBase."${serviceName}" // { name = serviceName; });
+  }) (builtins.attrNames servicesBase));
 }
