0af65dbc22cc03339d8bfe004d261c73e10fb348
Joseph Heyburn <34041368+jdheyburn@users.noreply.github.com>
2023-03-04 19:30:51 +0000

    Add macbook with nix-darwin (#43)
    * Initial commit of working nix-darwin + home-manager
    
    * Add formatter but commented
    
    * Add kubeswitch

--- home-manager/dotfiles/zsh-initExtra-misc.zsh
+++ home-manager/dotfiles/zsh-initExtra-misc.zsh
@@ -6,3 +6,14 @@ if [ -n "$\{commands[fzf-share]\}" ]; then
     source "$(fzf-share)/completion.zsh"
 fi
 
+# Load in rbenv (really only for macbook)
+if type rbenv &>/dev/null; then
+  eval "$(rbenv init -)"
+fi
+
+# kube-ps1 - if installed
+kube_ps1="/usr/local/opt/kube-ps1/share/kube-ps1.sh"
+if [ -f $kube_ps1 ]; then
+  source $kube_ps1
+  PS1='$(kube_ps1)'$PS1
+fi
--- home-manager/users/jdheyburn/default.nix
+++ home-manager/users/jdheyburn/default.nix
@@ -7,14 +7,31 @@
     userName = "Joseph Heyburn";
     userEmail = "jdheyburn@gmail.com";
     extraConfig = {
-      core = { pager = "${pkgs.diff-so-fancy}/bin/diff-so-fancy | less -RF"; };
+      core.pager = "${pkgs.diff-so-fancy}/bin/diff-so-fancy | less --tabs=4 -RFX";
+      init.defaultBranch = "main";
+      "url \"git@github.com:\"".insteadOf = "https://github.com/";
     };
   };
 
   programs.gh = {
     enable = true;
-    settings = { git_protocol = "ssh"; };
+    settings = {
+      # Aliases allow you to create nicknames for gh commands
+      aliases = {
+        co = "pr checkout";
+      };
+
+      # What editor gh should run when creating issues, pull requests, etc. If blank, will refer to environment.
+      editor = "!!null nvim";
+
+      # What protocol to use when performing git operations. Supported values: ssh, https
+      git_protocol = "ssh";
+
+      # A pager program to send command output to, e.g. "less". Set the value to "cat" to disable the pager.
+      pager = "";
+
+      # When to interactively prompt. This is a global config that cannot be overridden by hostname. Supported values: enabled, disabled
+      prompt = "enabled";
+    };
   };
-
 }
-
