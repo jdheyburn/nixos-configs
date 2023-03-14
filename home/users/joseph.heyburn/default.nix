34d4e2b0acc85eb60ae506b630ad013dd0c063bb
Joseph Heyburn <jdheyburn@gmail.com>
2023-03-14 00:01:44 +0000

    Add some more settings

--- home/users/joseph.heyburn/default.nix
+++ home/users/joseph.heyburn/default.nix
@@ -1,10 +1,21 @@
 { config, pkgs, ... }:
+let
 
+  terraform_0_14_10 = pkgs.mkTerraform {
+    version = "0.14.10";
+    hash = "sha256-3Ie19UfYpapBVvSTIFwE6Zm0X61FwMAJ7nio+iFabhc=";
+    vendorHash = "sha256-tWrSr6JCS9s+I0T1o3jgZ395u8IBmh73XGrnJidWI7U=";
+  };
+
+in
 {
-
   home.packages = with pkgs; [
     awscli2
     kubernetes-helm
+    python310
     sops
+    terraform_0_14_10
   ];
+
+  programs.direnv.enable = true;
 }
