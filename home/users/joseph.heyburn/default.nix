6759d31344cfc99aebc5dd09707e16c157764b92
Joseph Heyburn <jdheyburn@gmail.com>
2023-03-21 21:34:57 +0000

    Some typo fixes

--- home/users/joseph.heyburn/default.nix
+++ home/users/joseph.heyburn/default.nix
@@ -63,7 +63,7 @@ in
       ms-python.python
 
       # Material Design Icons for Visual Studio Code
-      # pkief.material-icon-theme
+      pkief.material-icon-theme
 
       # Ruby language support and debugging for Visual Studio Code
       # rebornix.ruby
@@ -76,6 +76,15 @@ in
 
       # Syntax highlighing, snippet, and language configuration support for Ruby
       # wingrunr21.vscode-ruby
+    ]
+    # Install other extension from the marketplace that aren't in nixpkgs
+    ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "vsc-material-theme";
+          publisher = "Equinusocio";
+          version = "33.7.0";
+          sha256 = "sha256-qwnu48dPjJN/wlaiwHS4SU3Yn4Y3GuOB1W+QoSjcgKw=";
+        }
     ];
 
     globalSnippets = {
@@ -85,6 +94,7 @@ in
         prefix = [ "todo" ];
       };
     };
+
     userSettings = {
       "[python]"."editor.formatOnType" = true;
 
@@ -108,12 +118,13 @@ in
 
       "python.showStartPage" = false;
 
-      "terminal.integrated.copyOnSeletion" = true;
+      "terminal.integrated.copyOnSelection" = true;
       "terminal.integrated.enableMultiLinePasteWarning" = false;
       "terminal.integrated.fontFamily" = "'Meslo LG M DZ for Powerline', monospace";
       "terminal.integrated.fontSize" = 12;
 
-      "workbench.colorTheme" = "Material Theme";
+      # Material theme seems to want to remove this config and to use its own instead
+      "workbench.colorTheme" = "Material Theme Darker";
       "workbench.iconTheme" = "material-icon-theme";
       "workbench.startupEditor" = "none";
     };
