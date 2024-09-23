f288c8a25ab1f77d2a1ce525fa5fd369b501c717
Joseph Heyburn <34041368+jdheyburn@users.noreply.github.com>
2024-09-23 08:10:49 +0100

    Home manager common refactor (#70)
    * Rename home/common to directory
    
    * Split home/common into smaller units

---
+++ home/common/default.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, lib, ... }: {
+
+  imports = [
+    ./cli
+    ./git
+    ./neovim
+    ./tmux
+    ./zsh
+  ];
+
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.05";
+
+  home.packages = with pkgs; [
+    delta
+    diff-so-fancy
+    dyff
+  ];
+
+  # Permit non-free software
+  nixpkgs.config.allowUnfree = true;
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # https://nix.catppuccin.com/options/home-manager-options.html
+  catppuccin = {
+    # Enable catppuccin themes whereever supported
+    enable = true;
+    flavor = "macchiato";
+  };
+
+}
---
+++ home/common/zsh/default.nix
@@ -0,0 +1,74 @@
+{ config, pkgs, lib, ... }: {
+  programs.zsh = {
+    enable = true;
+    autosuggestion.enable = true;
+    enableCompletion = true;
+    syntaxHighlighting.enable = true;
+
+    history = {
+      save = 10000000;
+      size = 10000000;
+      ignoreDups = false; # I use HIST_IGNORE_ALL_DUPS later on
+    };
+
+    shellAliases = {
+      awscf = "vi ~/.aws/config";
+      cl = "clear";
+      cp = "cp -Rv";
+      df = "${pkgs.duf}/bin/duf";
+      gpm = "git pull origin $(git_main_branch)";
+      h = "history";
+      kc = "kubectx";
+      kn = "kubens";
+      ls = "${pkgs.eza}/bin/eza";
+      mv = "mv -v";
+      nrs = "sudo nixos-rebuild switch";
+      venv = "source .venv/bin/activate";
+    };
+
+    shellGlobalAliases = {
+      # If cat is a global alias then we can pipe to bat easily enough
+      cat = "${pkgs.bat}/bin/bat";
+      # One character grep
+      G = "| grep -i ";
+      # Output to yaml then cat it, useful on k8s resources
+      YC = "-o yaml | cat";
+    };
+
+    # sessionVariables get prefixed with `export`
+    # localVariables do not
+    sessionVariables = {
+      DELTA_PAGER = "less --tabs=4 --RAW-CONTROL-CHARS --no-init --quit-if-one-screen";
+      EDITOR = "nvim";
+      SUDO_EDITOR = "nvim";
+    };
+
+    # This is executed before plugins such as oh-my-zsh are called
+    initExtraBeforeCompInit = ''
+      # Stops escaping URL characters, slow copy-paste, etc.
+      DISABLE_MAGIC_FUNCTIONS=true
+    '';
+
+    initExtra = ''
+      ${builtins.readFile ./zsh-initExtra-kubectl_aliases.zsh}
+      ${builtins.readFile ./zsh-initExtra-functions.zsh}
+      ${builtins.readFile ./zsh-initExtra-misc.zsh}
+    '';
+
+    oh-my-zsh = {
+      enable = true;
+      theme = "agnoster";
+      plugins =
+        [ "colored-man-pages" "sudo" ];
+    };
+
+    prezto = {
+      enable = false;
+      tmux = {
+        autoStartRemote = true;
+        defaultSessionName = "joe-test";
+      };
+    };
+  };
+}
+
