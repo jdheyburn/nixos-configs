839ff00af72255c39aafd27fee16143ccd6b8f32
Joseph Heyburn <jdheyburn@gmail.com>
2024-10-03 20:46:16 +0100

    Add desktop role

---
+++ home/roles/desktop/default.nix
@@ -0,0 +1,45 @@
+{ pkgs, ... }:
+
+let
+  tmuxSettings = {
+    RequestTTY = "yes";
+    RemoteCommand = "tmux new-session -A -s ssh_tmux";
+  };
+in
+{
+  home.packages = with pkgs; [
+    #  mullvad-vpn
+  ];
+
+  # SSH client related stuff here, I only want this on paddys (laptop)
+  services.ssh-agent.enable = true;
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      charlie = {
+        extraOptions = tmuxSettings;
+      };
+      charlie-no-tmux = {
+        hostname = "charlie";
+      };
+      dee = {
+        extraOptions = tmuxSettings;
+      };
+      dee-no-tmux = {
+        hostname = "dee";
+      };
+      dennis = {
+        extraOptions = tmuxSettings;
+      };
+      dennis-no-tmux = {
+        hostname = "dennis";
+      };
+      "gitlab.com" = {
+        user = "git";
+      };
+      "github.com" = {
+        user = "git";
+      };
+    };
+  };
+}
