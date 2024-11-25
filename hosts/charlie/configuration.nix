2d5e71545cbb036f2178a603a0b588af10c595de
Joseph Heyburn <jdheyburn@gmail.com>
2024-11-25 13:39:40 +0000

    Modularise remote builders

--- hosts/charlie/configuration.nix
+++ hosts/charlie/configuration.nix
@@ -43,16 +43,7 @@
   services.jellyfin.user = "jdheyburn";
   services.jellyfin.group = "users";
 
-  # For remote builds
-  ## Caddy cannot be built in a sandbox because it retrieves external dependencies (i.e. cloudflare-dns module)
-  nix.settings.sandbox = false;
-  ## Don't garbage collect nix builds from deploy-rs
-  ## Removing this will make failed deploys rebuild every time
-  nix.settings.keep-outputs = true;
-  nix.settings.keep-derivations = true;
-  ## Emulate building for aarch64 (Raspberry Pi)
-  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
-
+  modules.remote-builder.enable = true;
 
   # For tailscale exit nodes
   #boot.kernel.sysctl = {
--- hosts/mac/configuration.nix
+++ hosts/mac/configuration.nix
@@ -19,14 +19,12 @@
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
 
-  # For remote builds
-  ## Caddy cannot be built in a sandbox because it retrieves external dependencies (i.e. cloudflare-dns module)
-  nix.settings.sandbox = false;
-  ## Don't garbage collect nix builds from deploy-rs
-  ## Removing this will make failed deploys rebuild every time
-  nix.settings.keep-outputs = true;
-  nix.settings.keep-derivations = true;
-  ## Emulate building for aarch64 (Raspberry Pi)
-  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+
+  ###############################################################################
+  ## Modules
+  ###############################################################################
+
+  modules.remote-builder.enable = true;
+
 }
 
