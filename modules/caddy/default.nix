8c6ec8070d21098bd0a0e55e71d0195e9ed70059
Joseph Heyburn <34041368+jdheyburn@users.noreply.github.com>
2025-01-01 13:42:51 +0000

    Use upstream caddy (#89)
    * Initial commit of refactor backups
    
    - Working correctly on dee
    - Needs to be tested on dennis
    
    TODO:
    - Refactor backupSF module?
    - dee and dennis can backup to same repo (small-files) at the same time?
    - Reconsider backups to NFS then cloud, not copying NFS to cloud
    
    * Add paperless but disabled
    
    * Use upstream Caddy withPlugins
    
    * Init commit of domain catalog
    
    * Working reference
    
    * Use catalog for domain
    
    * Use catalog for tailscale domain
    
    * Working deploys
    
    * Merge
    
    * Deploy paperless to charlie
    
    * Try to fix dee crashing
    
    * Working upstream caddy

--- modules/caddy/default.nix
+++ modules/caddy/default.nix
@@ -33,9 +33,10 @@ in
 
     services.caddy = {
       enable = true;
-      package = (pkgs.callPackage ./custom-caddy.nix {
-        plugins = [ "github.com/caddy-dns/cloudflare" ];
-      });
+      package = pkgs.caddy.withPlugins {
+        plugins = [ "github.com/caddy-dns/cloudflare@v0.0.0-20240703190432-89f16b99c18e" ];
+        hash = "sha256-WGV/Ve7hbVry5ugSmTYWDihoC9i+D3Ct15UKgdpYc9U=";
+      };
     };
 
     systemd.services.caddy = {
