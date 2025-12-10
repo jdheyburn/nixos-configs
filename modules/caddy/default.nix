58af55ae9da075e06616de1530e7c689e8aedd40
Joseph Heyburn <jdheyburn@gmail.com>
2025-12-10 01:35:12 +0000

    Working on charlie

--- modules/caddy/default.nix
+++ modules/caddy/default.nix
@@ -35,7 +35,8 @@ in
       enable = true;
       package = pkgs.caddy.withPlugins {
         plugins = [ "github.com/caddy-dns/cloudflare@v0.2.1" ];
-        hash = "sha256-2D7dnG50CwtCho+U+iHmSj2w14zllQXPjmTHr6lJZ/A=";
+        #hash = "sha256-2D7dnG50CwtCho+U+iHmSj2w14zllQXPjmTHr6lJZ/A=";
+       hash = "sha256-Dvifm7rRwFfgXfcYvXcPDNlMaoxKd5h4mHEK6kJ+T4A=";
       };
     };
 
