61ad4cc0896dfd13c9196dd3efca948f44499853
Joseph Heyburn <jdheyburn@gmail.com>
2026-01-17 09:36:27 +0000

    Debug tmux fzf

--- home/common/cli/default.nix
+++ home/common/cli/default.nix
@@ -12,8 +12,6 @@
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
-    # Disabled: fzf-tmux fails to connect to tmux socket on Linux, causing
-    # "error connecting to /run/user/1000/tmux-1000/default" and terminal lock
     tmux.enableShellIntegration = true;
   };
   programs.zsh.oh-my-zsh.plugins = [ "fzf" ];
