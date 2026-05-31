7d15f71135cf6778a846ea8a0c7c6e625d938292
Joseph Heyburn <jdheyburn@gmail.com>
2026-05-31 09:05:53 +0100

    update hardwareconfig

--- hosts/nixos/dee/hardware-configuration.nix
+++ hosts/nixos/dee/hardware-configuration.nix
@@ -1,62 +1,21 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  hardware.raspberry-pi."4".fkms-3d.enable = true;
-  hardware.raspberry-pi."4".dwc2.enable = false;
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
 
-  boot.kernelParams = [
-    "usb-storage.quirks=174c:1156:u"
-    "8250.nr_uarts=1"
-    "console=ttyAMA0,115200"
-    "console=tty1"
-    "cma=128M"
-  ];
-
-  boot.initrd.availableKernelModules =
-    [ "xhci_pci" "uas" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "usbhid" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
-  boot.initrd.supportedFilesystems = [ "zfs" ];
-  boot.supportedFilesystems = [ "zfs" ];
 
-  boot.consoleLogLevel = 7;
-
-  fileSystems."/" = {
-    device = "rpool/root/nixos";
-    fsType = "zfs";
-  };
-
-  fileSystems."/home" = {
-    device = "rpool/home";
-    fsType = "zfs";
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/D36D-B744";
-    fsType = "vfat";
-  };
-
-  #fileSystems."/mnt/nfs" = {
-  #  device = "/dev/disk/by-uuid/242E77B52E777F1C";
-  #  fsType = "ntfs";
-  #  options = [ "nofail" ];
-  #};
-
-  fileSystems."/mnt/nfs" = {
-    device = "/dev/disk/by-uuid/5bc9d4ef-9379-4381-bfbd-dfe63a0575ea";
-    fsType = "ext4";
-    options = [ "nofail" ];
-  };
-
-  fileSystems."/mnt/usb" = {
-    device = "/mnt/nfs";
-    options = [ "bind" ];
-  };
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
+      fsType = "ext4";
+    };
 
   swapDevices = [ ];
 
-  powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
-  powerManagement.powertop.enable = true;
+  nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
 }
-
