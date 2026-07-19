2a980aa77d011ebc69683f0114ac0e910f3aec5e
Joseph Heyburn <jdheyburn@gmail.com>
2026-07-19 17:50:18 +0100

    Disable fan

--- hosts/nixos/dee/hardware-configuration.nix
+++ hosts/nixos/dee/hardware-configuration.nix
@@ -18,16 +18,17 @@
 
   hardware.i2c.enable = true;
   boot.kernelModules = [ "i2c-dev" "i2c-bcm2835" ];
+  boot.kernelParams = [ "dtparam=i2c_arm_baudrate=10000" ];
 
-  systemd.services.argon-fan-off = {
-    description = "Force Argon ONE fan off at boot";
-    after = [ "multi-user.target" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = "${pkgs.i2c-tools}/bin/i2cset -y 1 0x1a 0x00";
-    };
-  };
+  # systemd.services.argon-fan-off = {
+  #   description = "Force Argon ONE fan off at boot";
+  #   after = [ "multi-user.target" ];
+  #   wantedBy = [ "multi-user.target" ];
+  #   serviceConfig = {
+  #     Type = "oneshot";
+  #     ExecStart = "${pkgs.i2c-tools}/bin/i2cset -y 1 0x1a 0x00";
+  #   };
+  # };
 
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
