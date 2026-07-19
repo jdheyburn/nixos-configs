e4f55eb68b2e5e38c4e00a365270eaa67a55b16e
Joseph Heyburn <jdheyburn@gmail.com>
2026-07-19 18:22:59 +0100

    add usb drive

--- hosts/nixos/dee/hardware-configuration.nix
+++ hosts/nixos/dee/hardware-configuration.nix
@@ -35,6 +35,17 @@
       fsType = "ext4";
     };
 
+  fileSystems."/mnt/nfs" = {
+    device = "/dev/disk/by-uuid/5bc9d4ef-9379-4381-bfbd-dfe63a0575ea";
+    fsType = "ext4";
+    options = [ "nofail" ];
+  };
+
+  fileSystems."/mnt/usb" = {
+    device = "/mnt/nfs";
+    options = [ "bind" ];
+  };
+
   swapDevices = [ ];
 
   nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
