b03f6c16c3c3c2d38f42d184c91f3d40cd68daa5
Joseph Heyburn <jdheyburn@gmail.com>
2026-07-19 16:55:07 +0100

    correct hash

--- modules/nixos/caddy/default.nix
+++ modules/nixos/caddy/default.nix
@@ -35,7 +35,7 @@ in
       enable = true;
       package = pkgs.caddy.withPlugins {
         plugins = [ "github.com/caddy-dns/cloudflare@v0.2.4" ];
-        hash = "sha256-F7d4HwM4oCkQrFMr4SFSC0r52ONxY+PW6z5BJawW8Ok=";
+        hash = "sha256-7GoH8YLCoPmPExQxoga2FHB58zQDoZVf1BBwkVi0SsQ=";
       };
     };
 
